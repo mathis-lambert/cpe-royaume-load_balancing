a64a78c763a7bb24c358350c0105b623b45b08aa
Vincent C <vincent.chavot----dambrun@etu.univ-lyon1.fr>
2026-02-02 11:04:42 +0100

    add call API

--- src/main/java/com/cpe/royaume/service/RoyalService.java
+++ src/main/java/com/cpe/royaume/service/RoyalService.java
@@ -1,12 +1,17 @@
 package com.cpe.royaume.service;
 
+import com.cpe.royaume.entity.ApiResponse;
+import com.cpe.royaume.entity.Quest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class RoyalService {
+    private final Logger LOGGER = LoggerFactory.getLogger(RoyalService.class);
 
     private final WebClient webClient;
     private final String group;
@@ -21,24 +26,29 @@ public class RoyalService {
         this.group = group;
     }
 
-    public String getQuests() {
-        return webClient.get()
+    public Quest getQuests() {
+        ApiResponse response = webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/quests")
                         .queryParam("group", group)
                         .build())
                 .retrieve()
-                .bodyToMono(String.class)
+                .bodyToMono(ApiResponse.class)
                 .block();
+        LOGGER.debug("Response from getQuests: {}", response);
+        return response != null ? response.getQuest() : null;
     }
 
     public ApiResponse resolveQuest(final String questId) {
-        String url = ROYAUME_API_URL + "/quests/" + questId + "/resolve";
-        ResponseEntity<ApiResponse> response = restTemplate.postForEntity(
-                url,
-                null,
-                ApiResponse.class
-        );
-        return response.getBody();
+        ApiResponse response = webClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/quests/{id}/resolve")
+                        .queryParam("group", group)
+                        .build(questId))
+                .retrieve()
+                .bodyToMono(ApiResponse.class)
+                .block();
+        LOGGER.debug("Response from resolveQuest: {}", response);
+        return response;
     }
 }
