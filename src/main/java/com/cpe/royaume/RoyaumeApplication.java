b3008abf74118b500bae5dc5d92819bea6ec9669
Mathis LAMBERT <mathis.lambert@cpe.fr>
2026-02-02 09:28:14 +0100

    init: initialized project

---
+++ src/main/java/com/cpe/royaume/RoyaumeApplication.java
@@ -0,0 +1,13 @@
+package com.cpe.royaume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyaumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyaumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpe/royaume/RoyaumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpe.royaume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyaumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
